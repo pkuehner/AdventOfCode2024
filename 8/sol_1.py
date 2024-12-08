0b582bff45391d6a49becdfd986af40df2cc54b9
Pascal Kühner <pascal.kuehner@rwtn-aachen.de>
2024-12-08 12:49:13 +0100

    Improved sol_1

--- 8/sol_1.py
+++ 8/sol_1.py
@@ -2,55 +2,53 @@ from enum import Enum
 from typing import Tuple
 
 
-def search(x: int, y: int, arr, antidotes, negX: bool = False, negY: bool = False):
-    for offSetX in range(0, int(len(arr)/2) + 1):
-        for offSetY in range(0, int(len(arr[x])/2) + 1):
-            if offSetY == 0 and offSetX == 0:
-                continue
-            if negX:
-                xN = x - 2 * offSetX
-                xN2 = x - offSetX
-            else:
-                xN = x + 2 * offSetX
-                xN2 = x + offSetX
-            if negY:
-                yN = y - 2 * offSetY
-                yN2 = y - offSetY
-            else:
-                yN = y + 2 * offSetY
-                yN2 = y + offSetY
-            
+def search(x: int, y: int, arr, antidotes):
+    letter = arr[x][y]
+    if letter ==".":
+        return
 
-            if xN < 0 or xN >= len(arr):
+    for xN in range(len(arr)):
+        for yN in range(len(arr[x])):
+            letter_2 = arr[xN][yN]
+            if xN == x and yN == y:
                 continue
-            if yN < 0 or yN >= len(arr[xN]):
+            if letter_2 != letter:
                 continue
-            if xN2 < 0 or xN2 >= len(arr):
-                continue
-            if yN2 < 0 or yN2 >= len(arr[xN2]):
-                continue
-            letter = arr[xN][yN]
-            if letter != ".":
-                if arr[xN2][yN2] == letter:
-                    antidotes[x][y] = 1
-                    break
+            offSetX = xN-x
+            offSetY = yN-y
+            for i in [-1, 2]:
+                xToFill = x+i*offSetX
+                if xToFill < 0 or xToFill >= len(arr):
+                    continue
+                yToFill = y + i*offSetY
+                if yToFill < 0 or yToFill >= len(arr[0]):
+                    continue
+
+                antidotes[xToFill][yToFill] = 1  
+            
+                            
 
 
 with open("input") as file:
     text = file.readlines()
     arr = [list(line) for line in text]
     arr = [line[:-1] if line[-1] =="\n" else line for line in arr ]
+    print(arr)
     antidotes = [line.copy() for line in arr.copy()]
     result = 0
     for x in range(len(arr)):
         for y in range(len(arr[x])):
             antidotes[x][y] = 0
-            search(x, y, arr, antidotes, False, False)
-            search(x, y, arr, antidotes, True, False)
-            search(x, y, arr, antidotes, False, True)
-            search(x, y, arr, antidotes, True, True)
+    for x in range(len(arr)):
+        for y in range(len(arr[x])):
+            search(x, y, arr, antidotes)
+
 
     for x in range(len(arr)):
         for y in range(len(arr[x])):
             result += antidotes[x][y]
+            if antidotes[x][y] == 1:
+                #print(x,y)
+                pass
     print(result)
+
